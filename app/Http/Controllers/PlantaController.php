0e4564ac001047fe1ec4086cadee1829728b4515
Mai Pérez <maitreyiperez@gmail.com>
2023-10-13 17:29:14 -0600

    Comentarios explicando

--- app/Http/Controllers/PlantaController.php
+++ app/Http/Controllers/PlantaController.php
@@ -3,29 +3,35 @@
 namespace App\Http\Controllers;
 use App\Models\Planta;
 use Laravel\Lumen\Routing\Controller as BaseController;
+use Illuminate\Http\Request;
 
 class PlantaController extends BaseController
 {
+    //Esto nos devolverá todas las plantas
     function getAll(){
        $registros =  Planta::all();
        return response()->json($registros, 200);
     }
 
+    //Este solo una planta por id
     function getById($id){
         $planta =  Planta::find($id);
         return response()->json($planta, 200);
      }
-    function agregarPlanta($id,$nombre){
+
+    // Este agregará o editará una planta
+    function agregarPlanta(Request $request, $id = -1){
         //Request $request
         $planta = new Planta();
         if($id != -1){
             $planta = Planta::find($id);
         }
         //$planta->plan_id = $request->plan_id;
-        $planta->plan_nombre = $nombre;
+        $planta->plan_nombre = $request->nombre;
         $planta->save();
         return response()->json($planta,200);
     }
+    //Este borrará una planta
     function borrarPlanta($id){
         $planta = Planta::find($id);
 
--- routes/web.php
+++ routes/web.php
@@ -13,12 +13,88 @@
 |
 */
 
+// Aquí se declaran todas las rutas que se van a utilizar. 
+// siendo http://localhost:8000/ el inicio, y la continuación lo que se declare abajo.
+
+//Esta es la pagina que cargará de manera automatica cuando solo se ponga la dirección (http://localhost:8000/)
 $router->get('/', function () use ($router) {
-    return $router->app->version();
+    //return $router->app->version();
+    return "
+    
+    <!DOCTYPE html>
+<html lang=\"es\">
+<head>
+    <meta charset=\"UTF-8\">
+    <meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">
+    <title>Pantalla de Inicio</title>
+    <style>
+        body {
+            font-family: Arial, sans-serif;
+            margin: 20px;
+        }
+        .section {
+            padding: 20px;
+            margin-bottom: 20px;
+            border: 1px solid #ccc;
+            border-radius: 10px;
+        }
+    </style>
+</head>
+<body>
+<h1>Hola api de plantas</h1>
+<div class=\"section\">
+    <h2>Sección 1: ¿Que es una api?</h2>
+    <p>na API, que significa \"Interfaz de Programación de Aplicaciones\" en inglés, es como un camarero en un restaurante.</p>
+    <p> Imagina que tienes un restaurante (tu videojuego) y quieres servir platos deliciosos (datos o funcionalidades) a tus clientes (usuarios del videojuego).</p>
+    <ol>
+        <li>GET Endpoint: Retorna datos específicos de la API.</li>
+        <li>POST Endpoint: Permite agregar nuevos datos a la API.</li>
+        <li>PUT Endpoint: Modifica datos existentes en la API.</li>
+        <li>DELETE Endpoint: Elimina datos de la API.</li>
+    </ol>
+
+</div>
+
+<div class=\"section\">
+    <h2>Sección 2: ¿Que es un endpoint?</h2>
+    <p>Es como un link donde se enviaran y recibiran datos en el formato json</p>
+    <p>
+    Aquí está un ejemplo de un objeto JSON:
+    <pre>
+        {
+            \"nombre\": \"Juanito\",
+            \"edad\": 25,
+            \"ciudad\": \"San Salvador\"
+        }
+    </pre>
+    </p>
+</div>
+
+<div class=\"section\">
+    <h2>Sección 3: ¿Que es consumir una api?</h2>
+    <p>Es utilizar los endpoints para recibir una respuesta</p>
+    <p>Un buen ejemplo de varios endpoints de una api es la api de pokemon</p>
+    <a href= https://pokeapi.co/> https://pokeapi.co/</a>
+</div>
+
+</body>
+</html>
+";
 });
 
 
+// Todas las plantas
 $router->get('/plantas', 'PlantaController@getAll');
+
+//Una planta
 $router->get('/plantas/{id}', 'PlantaController@getById');
-$router->get('/plantas/save/{id}/{nombre}', 'PlantaController@agregarPlanta');
-$router->get('/plantas/delete/{id}', 'PlantaController@borrarPlanta');
+
+// Guardar o modificar planta, sin id, creará una nueva.
+$router->post('/plantas/save/', 'PlantaController@agregarPlanta');
+
+// Este puede ser PUT así como puede ser SAVE por la manera que está hecha la función.
+$router->post('/plantas/save/{id}', 'PlantaController@agregarPlanta');
+$router->put('/plantas/save/{id}', 'PlantaController@agregarPlanta');
+
+// Borrar una planta
+$router->delete('/plantas/delete/{id}', 'PlantaController@borrarPlanta');
